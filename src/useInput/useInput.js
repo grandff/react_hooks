7772871a88f9b15586782b640c66448a029c62ca
김정민 <dsaqaz421@naver.com>
2020-12-10 11:45:50 +0900

    useInput study

---
+++ src/useInput/useInput.js
@@ -0,0 +1,33 @@
+// useInput + validator
+
+import { useState } from "react"
+
+export const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event => {
+        const { target : {value}} = event;
+        let willUpdate = true;
+        if (typeof validator === "function") willUpdate = validator(value);
+        if (willUpdate) setValue(value);        
+    };
+
+    return {value, onChange};
+}
+
+
+/*
+    use example
+
+    App.js or something
+    const maxLen = (value) => value.length <= 10;
+    const name = useInput("Mr. ", maxLen);
+
+    return (
+        <div className="App">
+        <h1>Hello</h1>
+        <input placeholder="Name" {...name} />
+        </div>
+    );
+
+
+*/
